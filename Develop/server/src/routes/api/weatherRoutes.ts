baf3e08c195544a31a3ed5cffbc6ac86ef2a7eed
shukikat <kathuriashuki@gmail.com>
2024-09-18 22:59:31 -0400

    additonal code updates service files

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
+
 // import WeatherService from '../../service/weatherService.js';
+
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req, res) => {
   // TODO: GET weather data from city name
+  
   // TODO: save city to search history
 });
 
